fac3b3f00662b3cb5dc8c586c64e8695f582547b
Chris Fregly <dev@chrisfregly.com>
2022-06-22 00:14:18 -0700

    update spacing

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -94,109 +94,111 @@ export default function GamePage() {
     }, []);
 
     return (
-        <div ref={ref} className="h-full flex flex-col items-center select-none">
-            {/* TAILWIND HACK - Tailwind preloads only used classes so anything not in initial render will not work */}
-            <div className="text-red-500 text-blue-500 text-green-500 text-yellow-500 text-orange-500 text-pink-500 text-purple-500 text-teal-500"/>
-            <div className="border-red-500 border-blue-500 border-green-500 border-yellow-500 border-orange-500 border-pink-500 border-purple-500 border-teal-500"/>
-            <div className="bg-red-500 bg-blue-500 bg-green-500 bg-yellow-500 bg-orange-500 bg-pink-500 bg-pink-500 bg-purple-500 bg-teal-500"/>
-            <div className="fill-red-500 fill-blue-500 fill-green-500 fill-yellow-500 fill-orange-500 fill-pink-500 fill-pink-500 fill-purple-500 fill-teal-500"/>
-            {/* END HACK */}
-            <div className="relative w-full mb-1 justfy-self-start font-thin text-sm">
-                Share this link with friends:&nbsp;
-                <span className="underline cursor-pointer" onClick={() => {
-                    setCopied(1);
-                    navigator.clipboard.writeText(`${ window.location.protocol }//${ window.location.host }/${ gid }`)
-                }}>
-                    { `${ window.location.protocol }//${ window.location.host }/${ gid }` }
-                </span>
-                {
-                    copied > 0 ?
-                        <div className="absolute mt-2 w-full flex justify-center">
-                            <div className="absolute top-[-12px] w-6 overflow-hidden inline-block">
-                                <div className=" h-4 w-4 bg-zinc-600 rotate-45 transform origin-bottom-left" />
-                            </div>
-                            <div className="font-bold text-xs text-center bg-zinc-600 px-2 py-1">copied!</div>
-                        </div> : null
-                }
-            </div>
-            <hr className="w-full mb-2"/>
-            <div className="flex w-full justify-between items-center mb-4">
-                <div className="flex">
-                    { game ? game.Teams.map(el => <div key={ el } className={ `text-xs flex items-center justify-center font-bold cursor-pointer mr-1 w-6 h-6 rounded-full border-4 border-${ el }-500 ${ network && connected && connected[network.Name] === el  ? `bg-${ connected[network.Name] }-500` : "" }` } onClick={ () => setTeam(el) }>{ game && ["LongestPath", "MostCrossings"].includes(game.MoreData.Variant) ? game.MoreData.Points[el] : "" }</div>) : null }
-                </div>
-                <div className={ `font-extrabold ${ game && connected && network && connected[network.Name] && game.Winners.length === 0 ? `text-${ game.Turn }-500` : "text-zinc-100" }` }>
-                    { 
-                        game && connected && network && connected[network.Name] ? 
-                            game.Message : 
-                            <div className="flex items-center">
-                                <BsArrowLeft className="mr-1" />
-                                <div>select a team</div>
-                            </div>
+        <div className="min-h-screen flex flex-col items-center p-2 md:p-4">
+            <div ref={ref} className="h-full w-full flex flex-col items-center max-w-xl grow">
+                {/* TAILWIND HACK - Tailwind preloads only used classes so anything not in initial render will not work */}
+                <div className="text-red-500 text-blue-500 text-green-500 text-yellow-500 text-orange-500 text-pink-500 text-purple-500 text-teal-500"/>
+                <div className="border-red-500 border-blue-500 border-green-500 border-yellow-500 border-orange-500 border-pink-500 border-purple-500 border-teal-500"/>
+                <div className="bg-red-500 bg-blue-500 bg-green-500 bg-yellow-500 bg-orange-500 bg-pink-500 bg-pink-500 bg-purple-500 bg-teal-500"/>
+                <div className="fill-red-500 fill-blue-500 fill-green-500 fill-yellow-500 fill-orange-500 fill-pink-500 fill-pink-500 fill-purple-500 fill-teal-500"/>
+                {/* END HACK */}
+                <div className="relative w-full mb-1 justfy-self-start font-thin text-sm">
+                    Share this link with friends:&nbsp;
+                    <span className="underline cursor-pointer" onClick={() => {
+                        setCopied(1);
+                        navigator.clipboard.writeText(`${ window.location.protocol }//${ window.location.host }/${ gid }`)
+                    }}>
+                        { `${ window.location.protocol }//${ window.location.host }/${ gid }` }
+                    </span>
+                    {
+                        copied > 0 ?
+                            <div className="absolute mt-2 w-full flex justify-center">
+                                <div className="absolute top-[-12px] w-6 overflow-hidden inline-block">
+                                    <div className=" h-4 w-4 bg-zinc-600 rotate-45 transform origin-bottom-left" />
+                                </div>
+                                <div className="font-bold text-xs text-center bg-zinc-600 px-2 py-1">copied!</div>
+                            </div> : null
                     }
                 </div>
-            </div>
-
-            <DndProvider backend={ isMobile ? TouchBackend : HTML5Backend }>
-                <div className="h-full flex flex-col justify-center items-center grow">
-                    <div className="box-border flex flex-col mb-4" style={{ width: `${ tileSize*6 }px`, height: `${ tileSize*6 }px` }}>
+                <hr className="w-full mb-2"/>
+                <div className="flex w-full justify-between items-center mb-4">
+                    <div className="flex">
+                        { game ? game.Teams.map(el => <div key={ el } className={ `text-xs flex items-center justify-center font-bold cursor-pointer mr-1 w-6 h-6 rounded-full border-4 border-${ el }-500 ${ network && connected && connected[network.Name] === el  ? `bg-${ connected[network.Name] }-500` : "" }` } onClick={ () => setTeam(el) }>{ game && ["LongestPath", "MostCrossings"].includes(game.MoreData.Variant) ? game.MoreData.Points[el] : "" }</div>) : null }
+                    </div>
+                    <div className={ `font-extrabold ${ game && connected && network && connected[network.Name] && game.Winners.length === 0 ? `text-${ game.Turn }-500` : "text-zinc-100" }` }>
                         { 
-                            game ? game.MoreData.Board.map((row, rIdx) => 
-                                <div key={ rIdx } className="w-full h-full flex">
-                                    {
-                                        row.map((el, cIdx) => 
-                                            <DropSpace key={ cIdx } row={ rIdx } col={ cIdx }>
-                                                <div className="box-border border border-zinc-100" style={{ width: `${tileSize}px`, height: `${tileSize}px` }}>
-                                                    { 
-                                                        el ? 
-                                                            <Tile edges={ el.Edges } paths={ el.Paths } row={ rIdx } col={ cIdx } tokens={ game.MoreData.Tokens } team={ null } placeTile={ () => {} }/> : 
-                                                            <Tile edges={ null } paths={ null } row={ rIdx } col={ cIdx } tokens={ game.MoreData.Tokens } team={ null } placeTile={ () => {} }/> 
-                                                    }
-                                                </div>
-                                            </DropSpace>) 
-                                    }
-                                </div>) : null
+                            game && connected && network && connected[network.Name] ? 
+                                game.Message : 
+                                <div className="flex items-center">
+                                    <BsArrowLeft className="mr-1" />
+                                    <div>select a team</div>
+                                </div>
                         }
                     </div>
+                </div>
 
-                    <div className="mb-4 w-full flex justify-between items-center" style={{ height: `${tileSize}px` }}>
-                        <div className="flex flex-col items-center text-zinc-400 max-w-[20%]">
-                            <div className="text-xs font-light italic mb-1 text-center">Click tile to rotate</div>
-                            <BsArrowClockwise />    
-                        </div>       
-                        {
-                            [0, 1, 2].map((_, idx) => 
-                                <div key={ idx } className="box-border border border-zinc-100" style={{ width: `${ tileSize }px`, height: `${ tileSize }px` }}>
-                                    { 
-                                        game && network && connected && game.MoreData.Hands[connected[network.Name]] && game.MoreData.Hands[connected[network.Name]].length > idx ? 
-                                            <div className="cursor-pointer" onClick={ () => rotateTile(connected[network.Name], game.MoreData.Hands[connected[network.Name]][idx].Edges) }>
-                                                <Tile edges={ game.MoreData.Hands[connected[network.Name]][idx].Edges } paths={ game.MoreData.Hands[connected[network.Name]][idx].Paths } row={ -1 } col={ -1 } tokens={ {} } team={ connected[network.Name] } placeTile={ placeTile } />
-                                            </div> : 
-                                            null
-                                    }
-                                </div>) 
-                        }
-                        <div className="flex flex-col items-center text-zinc-400 max-w-[20%]">
-                            <div className="text-xs font-light italic mb-1 text-center">Drag tile to place</div>
-                            <BsArrowUp />    
+                <DndProvider backend={ isMobile ? TouchBackend : HTML5Backend }>
+                    <div className="h-full flex flex-col justify-center items-center grow">
+                        <div className="box-border flex flex-col mb-4" style={{ width: `${ tileSize*6 }px`, height: `${ tileSize*6 }px` }}>
+                            { 
+                                game ? game.MoreData.Board.map((row, rIdx) => 
+                                    <div key={ rIdx } className="w-full h-full flex">
+                                        {
+                                            row.map((el, cIdx) => 
+                                                <DropSpace key={ cIdx } row={ rIdx } col={ cIdx }>
+                                                    <div className="box-border border border-zinc-100" style={{ width: `${tileSize}px`, height: `${tileSize}px` }}>
+                                                        { 
+                                                            el ? 
+                                                                <Tile edges={ el.Edges } paths={ el.Paths } row={ rIdx } col={ cIdx } tokens={ game.MoreData.Tokens } team={ null } placeTile={ () => {} }/> : 
+                                                                <Tile edges={ null } paths={ null } row={ rIdx } col={ cIdx } tokens={ game.MoreData.Tokens } team={ null } placeTile={ () => {} }/> 
+                                                        }
+                                                    </div>
+                                                </DropSpace>) 
+                                        }
+                                    </div>) : null
+                            }
+                        </div>
+
+                        <div className="mb-4 w-full flex justify-between items-center" style={{ height: `${tileSize}px` }}>
+                            <div className="flex flex-col items-center text-zinc-400 max-w-[20%]">
+                                <div className="text-xs font-light italic mb-1 text-center">Click tile to rotate</div>
+                                <BsArrowClockwise />    
+                            </div>       
+                            {
+                                [0, 1, 2].map((_, idx) => 
+                                    <div key={ idx } className="box-border border border-zinc-100" style={{ width: `${ tileSize }px`, height: `${ tileSize }px` }}>
+                                        { 
+                                            game && network && connected && game.MoreData.Hands[connected[network.Name]] && game.MoreData.Hands[connected[network.Name]].length > idx ? 
+                                                <div className="cursor-pointer" onClick={ () => rotateTile(connected[network.Name], game.MoreData.Hands[connected[network.Name]][idx].Edges) }>
+                                                    <Tile edges={ game.MoreData.Hands[connected[network.Name]][idx].Edges } paths={ game.MoreData.Hands[connected[network.Name]][idx].Paths } row={ -1 } col={ -1 } tokens={ {} } team={ connected[network.Name] } placeTile={ placeTile } />
+                                                </div> : 
+                                                null
+                                        }
+                                    </div>) 
+                            }
+                            <div className="flex flex-col items-center text-zinc-400 max-w-[20%]">
+                                <div className="text-xs font-light italic mb-1 text-center">Drag tile to place</div>
+                                <BsArrowUp />    
+                            </div>
                         </div>
                     </div>
-                </div>
-            </DndProvider>
+                </DndProvider>
 
-            <hr className="w-full mb-2"/>
-            <div className="w-full flex justify-between items-center">
-                <div className="title leading-4 text-2xl font-black text-red-600 cursor-pointer">
-                    <a href={ `${ window.location.protocol }//${ window.location.host }` }>
-                        Tsuro
-                        <span className="ml-1 raleway text-[0.5rem] md:text-xs text-zinc-100">{ game ? game.MoreData.Variant : "" }</span>
-                    </a>
-                </div>
-                <div className="flex">
-                    <div className="flex">
-                        <div className="px-3 py-1 font-bold cursor-pointer flex items-center justify-center text-xs bg-zinc-600 mr-2" onClick={ () => resetGame() }>new game</div>
+                <hr className="w-full mb-2"/>
+                <div className="w-full flex justify-between items-center">
+                    <div className="title leading-4 text-2xl font-black text-red-600 cursor-pointer">
+                        <a href={ `${ window.location.protocol }//${ window.location.host }` }>
+                            Tsuro
+                            <span className="ml-1 raleway text-[0.5rem] md:text-xs text-zinc-100">{ game ? game.MoreData.Variant : "" }</span>
+                        </a>
                     </div>
-                    <div className="italic text-xs bg-blue-500 py-1 px-2">
-                        <a href="https://quibbble.com">more <span className="quibbble text-sm not-italic">quibbble</span> games</a>
+                    <div className="flex">
+                        <div className="flex">
+                            <div className="px-3 py-1 font-bold cursor-pointer flex items-center justify-center text-xs bg-zinc-600 mr-2" onClick={ () => resetGame() }>new game</div>
+                        </div>
+                        <div className="italic text-xs bg-blue-500 py-1 px-2">
+                            <a href="https://quibbble.com">more <span className="quibbble text-sm not-italic">quibbble</span> games</a>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,6 +3,7 @@ import { Adjectives, Nouns } from "./Words";
 import { CreateGame } from "../api/API";
 import { useHistory } from "react-router-dom"
 import { IoMdInformationCircleOutline } from "react-icons/io"
+import Footer from "./Footer";
 
 export default function HomePage() {
     const history = useHistory();
@@ -18,7 +19,7 @@ export default function HomePage() {
         if (status === 201 || status === 400) history.push(`/${ gid }`);
     }
     return (
-        <div className="flex flex-col items-center h-full">
+        <div className="flex flex-col items-center m-8 md:m-12">
             {
                 showVariantPopop ? 
                     <div className="absolute w-full h-full top-0 bg-zinc-500 bg-opacity-50 flex items-center justify-center fade-in">
@@ -33,39 +34,39 @@ export default function HomePage() {
                         </div>
                     </div> : <></>
             }
-            <div className="flex flex-col items-center">
-                <div className="title text-5xl font-black text-red-600 mb-1 cursor-pointer">
-                    <a href={ `${ window.location.protocol }//${ window.location.host }` }>Tsuro</a>
-                </div>
-                <div className="font-thin mb-3">
-                    Play two to eight player Tsuro online against friends.
-                    To create a game or join an existing one, enter a game ID and click 'Go'.
-                </div>
-                <form className="w-full flex mb-2" onSubmit={ handleGo }>
-                    <input className="w-10/12 p-2 text-zinc-100 bg-zinc-800 rounded-none border border-zinc-100 text-3xl font-medium box-border focus:outline-dashed outline-blue-500 outline-2" autoFocus type="text" value={ gid } onChange={ e => setGameID(e.target.value) }/>
-                    <button className="w-2/12 font-bold grow-0 bg-blue-500">Go</button>
-                </form>
-                <div className="flex w-full justify-between flex-wrap">
-                    <div className="italic text-xs bg-blue-500 py-1 px-2 order-2 md:order-1">
-                        <a href="https://quibbble.com">more <span className="quibbble text-sm not-italic">quibbble</span> games</a>
+            <div className="w-full max-w-2xl">
+                <div className="flex flex-col items-center fade-in">
+                    <div className="title text-5xl font-black text-red-600 mb-1 cursor-pointer">
+                        <a href={ `${ window.location.protocol }//${ window.location.host }` }>Tsuro</a>
                     </div>
-                    <div className="flex items-center order-1 md:order-2 mb-2 md:mb-0">
-                        <IoMdInformationCircleOutline className="mr-1 text-xl cursor-pointer" onClick={() => setShowVariantPopup(true)}/>
-                        <div className="mr-1 font-black text-blue-500">VARIANT</div>
-                        <select className="mr-1 bg-zinc-800 text-xs h-6 border font-bold border-zinc-100 focus:outline-none" id="players" onChange={ e => setVariant(e.target.value.replace(/\s/g, "")) }>
-                            { ["Classic", "Longest Path", "Most Crossings", "Open Tiles", "Solo"].map(el => <option key={ el } value={ el }>{ el }</option>) }
-                        </select>
-                        <div className="mx-1 font-black text-blue-500">{ variant === "Solo" ? "TOKENS" : "PLAYERS" }</div>
-                        <select className="bg-zinc-800 text-xs h-6 border font-bold border-zinc-100 focus:outline-none" id="players" onChange={ e => setTeams(parseInt(e.target.value)) }>
-                            { [2, 3, 4, 5, 6, 7, 8].map(el => <option key={ el } value={ el }>{ el }</option>) }
-                        </select>
+                    <div className="font-thin mb-3">
+                        Play two to eight player Tsuro online against friends.
+                        To create a game or join an existing one, enter a game ID and click 'Go'.
+                    </div>
+                    <form className="w-full flex mb-2" onSubmit={ handleGo }>
+                        <input className="w-10/12 p-2 text-zinc-100 bg-zinc-800 rounded-none border border-zinc-100 text-3xl font-medium box-border focus:outline-dashed outline-blue-500 outline-2" autoFocus type="text" value={ gid } onChange={ e => setGameID(e.target.value) }/>
+                        <button className="w-2/12 font-bold grow-0 bg-blue-500">Go</button>
+                    </form>
+                    <div className="flex w-full justify-between flex-wrap">
+                        <div className="italic text-xs bg-blue-500 py-1 px-2 order-2 md:order-1">
+                            <a href="https://quibbble.com">more <span className="quibbble text-sm not-italic">quibbble</span> games</a>
+                        </div>
+                        <div className="flex items-center order-1 md:order-2 mb-2 md:mb-0">
+                            <IoMdInformationCircleOutline className="mr-1 text-xl cursor-pointer" onClick={() => setShowVariantPopup(true)}/>
+                            <div className="mr-1 font-black text-blue-500">VARIANT</div>
+                            <select className="mr-1 bg-zinc-800 text-xs h-6 border font-bold border-zinc-100 focus:outline-none" id="players" onChange={ e => setVariant(e.target.value.replace(/\s/g, "")) }>
+                                { ["Classic", "Longest Path", "Most Crossings", "Open Tiles", "Solo"].map(el => <option key={ el } value={ el }>{ el }</option>) }
+                            </select>
+                            <div className="mx-1 font-black text-blue-500">{ variant === "Solo" ? "TOKENS" : "PLAYERS" }</div>
+                            <select className="bg-zinc-800 text-xs h-6 border font-bold border-zinc-100 focus:outline-none" id="players" onChange={ e => setTeams(parseInt(e.target.value)) }>
+                                { [2, 3, 4, 5, 6, 7, 8].map(el => <option key={ el } value={ el }>{ el }</option>) }
+                            </select>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="grow flex flex-col w-full h-full justify-end items-center">
-                <div className="font-thin text-sm italic">
-                    <a href="https://www.buymeacoffee.com/quibbble" target="_blank" rel="noreferrer">support the developer</a>
-                </div>
+            <div className="absolute bottom-8 md:bottom-12">
+                <Footer />
             </div>
         </div>
     )
