472637e8ac9d14c04305d2da43db5401ac2eec02
Chris Fregly <dev@chrisfregly.com>
2023-09-15 15:51:08 -0700

    use dndkit

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,35 +1,62 @@
 import React, { useState, useEffect, forwardRef, useCallback } from "react";
 import { BsArrowClockwise, BsArrowUp } from "react-icons/bs";
-import { DndProvider } from "react-dnd";
 import DropSpace from "./DropSpace";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import Tile from "./Tile";
-import { TouchBackend } from "react-dnd-touch-backend";
-import { isMobile } from "react-device-detect";
+import { DraggableTile, Tile } from "./Tile";
+import { DndContext, PointerSensor, useSensors, useSensor } from '@dnd-kit/core';
 
 export const Game = forwardRef((props, ref) => {
     // eslint-disable-next-line no-unused-vars
     const { ws, game, network, chat, connected, error } = props;
 
-    const placeTile = (team, row, col, tile) => {
+    // websocket messages
+    const sendSetTeamAction = useCallback((team) => {
         if (!ws.current) return;
-        ws.current.send(JSON.stringify({"ActionType": "PlaceTile", "Team": team, "MoreDetails": {"Row": row, "Column": col, "Tile": tile}}));
-    }
+        ws.current.send(JSON.stringify({"ActionType": "SetTeam", "MoreDetails": {"Team": team}}));
+    }, [ws])
 
-    const rotateTile = (team, tile) => {
+    const sendPlaceTileAction = useCallback((team, row, col, tile) => {
+        if (!ws.current) return;
+        console.log({"ActionType": "PlaceTile", "Team": team, "MoreDetails": {"Row": row, "Column": col, "Tile": tile}})
+        ws.current.send(JSON.stringify({"ActionType": "PlaceTile", "Team": team, "MoreDetails": {"Row": row, "Column": col, "Tile": tile}}));
+    }, [ws])
+
+    const sendRotateTileAction = useCallback((team, tile) => {
         if (!ws.current) return;
         ws.current.send(JSON.stringify({"ActionType": "RotateTileRight", "Team": team, "MoreDetails": {"Tile": tile}}));
-    }
+    }, [ws])
 
+    // game data
+    const [team, setTeam] = useState();
     useEffect(() => {
-        const setTeam = (team) => {
-            if (!ws.current) return;
-            ws.current.send(JSON.stringify({"ActionType": "SetTeam", "MoreDetails": {"Team": team}}));
+        if (connected && network) setTeam(connected[network.Name])
+    }, [connected, network])
+
+    const [hand, setHand] = useState([]);
+    useEffect(() => {
+        if (!(team && game && game.MoreData && game.MoreData.Hands && game.MoreData.Hands[team])) return 
+        setHand(game.MoreData.Hands[team])
+    }, [game, team])
+
+    // change teams every turn for solo variant
+    useEffect(() => {
+        if (team && game && game.MoreData.Variant === "Solo" && team !== game.Turn) {
+            sendSetTeamAction(game.Turn)
         }
-        if (game && network && connected && game.MoreData.Variant === "Solo" && connected[network.Name] !== game.Turn) {
-            setTeam(game.Turn)
-        }
-    }, [game, network, connected, ws])
+    }, [game, team, sendSetTeamAction])
+
+    // drag and drop
+    const sensors = useSensors(
+        useSensor(PointerSensor, {
+            activationConstraint: {
+                distance: 8,
+            },
+        })
+    )
+
+    const handleDragEnd = useCallback((e) => {
+        if (!e.over || team !== game.Turn || game.Winners.length > 0) return
+        sendPlaceTileAction(team, e.over.data.current.row, e.over.data.current.col, e.active.data.current.edges)
+    }, [team, game, sendPlaceTileAction])
 
     // board resize logic
     const [tileSize, setTileSize] = useState(0);
@@ -48,7 +75,7 @@ export const Game = forwardRef((props, ref) => {
     }, [handleResize]);
 
     return (
-        <DndProvider backend={ isMobile ? TouchBackend : HTML5Backend }>
+        <DndContext onDragEnd={ handleDragEnd } sensors={ sensors }>
             <div className="h-full flex flex-col justify-center items-center grow">
                 <div className="box-border flex flex-col mb-4" style={{ width: `${ tileSize*6 }px`, height: `${ tileSize*6 }px` }}>
                     { 
@@ -60,8 +87,8 @@ export const Game = forwardRef((props, ref) => {
                                             <div className="box-border border border-zinc-100" style={{ width: `${tileSize}px`, height: `${tileSize}px` }}>
                                                 { 
                                                     el ? 
-                                                        <Tile edges={ el.Edges } paths={ el.Paths } row={ rIdx } col={ cIdx } tokens={ game.MoreData.Tokens } team={ null } placeTile={ () => {} }/> : 
-                                                        <Tile edges={ null } paths={ null } row={ rIdx } col={ cIdx } tokens={ game.MoreData.Tokens } team={ null } placeTile={ () => {} }/> 
+                                                        <Tile edges={ el.Edges } paths={ el.Paths } row={ rIdx } col={ cIdx } tokens={ game.MoreData.Tokens } /> : 
+                                                        <Tile edges={ null } paths={ null } row={ rIdx } col={ cIdx } tokens={ game.MoreData.Tokens } /> 
                                                 }
                                             </div>
                                         </DropSpace>) 
@@ -71,7 +98,7 @@ export const Game = forwardRef((props, ref) => {
                 </div>
 
                 <div className="mb-4 w-full flex justify-between items-center" style={{ height: `${tileSize}px` }}>
-                    <div className="flex flex-col items-center text-zinc-400 max-w-[20%]">
+                    <div className="flex flex-col items-center text-zinc-400 max-w-[20%] select-none">
                         <div className="text-xs font-light italic mb-1 text-center">Click tile to rotate</div>
                         <BsArrowClockwise />    
                     </div>       
@@ -79,19 +106,18 @@ export const Game = forwardRef((props, ref) => {
                         [0, 1, 2].map((_, idx) => 
                             <div key={ idx } className="box-border border border-zinc-100" style={{ width: `${ tileSize }px`, height: `${ tileSize }px` }}>
                                 { 
-                                    game && network && connected && game.MoreData.Hands[connected[network.Name]] && game.MoreData.Hands[connected[network.Name]].length > idx ? 
-                                        <div className="cursor-pointer" onClick={ () => rotateTile(connected[network.Name], game.MoreData.Hands[connected[network.Name]][idx].Edges) }>
-                                            <Tile edges={ game.MoreData.Hands[connected[network.Name]][idx].Edges } paths={ game.MoreData.Hands[connected[network.Name]][idx].Paths } row={ -1 } col={ -1 } tokens={ {} } team={ connected[network.Name] } placeTile={ placeTile } />
-                                        </div> : 
-                                        null
+                                    hand.length > idx ? 
+                                        <div className="cursor-pointer" onClick={ () => game.Winners.length === 0 ? sendRotateTileAction(team, hand[idx].Edges) : null }>
+                                            { <DraggableTile id={ idx+1 } edges={ hand[idx].Edges } paths={ hand[idx].Paths } row={ -1 } col={ -1 } tokens={ {} } /> }
+                                        </div> : null
                                 }
                             </div>) 
                     }
-                    <div className="flex flex-col items-center text-zinc-400 max-w-[20%]">
+                    <div className="flex flex-col items-center text-zinc-400 max-w-[20%] select-none">
                         <div className="text-xs font-light italic mb-1 text-center">Drag tile to place</div>
                         <BsArrowUp />    
                     </div>
                 </div>
             </div>
-        </DndProvider>
+        </DndContext>
 )})
--- src/components/game/Tile.jsx
+++ src/components/game/Tile.jsx
@@ -1,9 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, forwardRef } from "react";
 import Notch from "./Notch";
 import Edge from "./Edge";
-import { useDrag } from "react-dnd";
+import { useDraggable } from '@dnd-kit/core';
 
-export default function Tile({ edges, paths, row, col, tokens, team, placeTile }) {
+export function DraggableTile({ id, edges, paths, row, col, tokens }) {
+    const {attributes, isDragging, listeners, setNodeRef, transform} = useDraggable({
+        id: id,
+        data: {
+            edges: edges
+        }
+    });
+
+    const style = {
+        opacity: isDragging ? 0.9 : undefined,
+        touchAction: "none",
+        transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined
+    }
+
+    return (
+        <Tile ref={ setNodeRef } style={ style } 
+            {...attributes} {...listeners}
+            edges={edges} paths={paths} row={row} col={col} tokens={tokens} />
+    )
+}
+
+export const Tile = forwardRef(({ edges, paths, row, col, tokens, ...props }, ref) => {    
     const [notches, setNotches] = useState([]);
     useEffect(() => {
         let temp = [];
@@ -13,35 +34,20 @@ export default function Tile({ edges, paths, row, col, tokens, team, placeTile }
         setNotches(temp)
     }, [row, col, tokens]);
 
-    const [{opacity}, drag, preview] = useDrag(() => ({
-        type: "tile",
-        item: { edges },
-        canDrag: () => row < 0 && col < 0,
-        end: (item, monitor) => {
-            const dropResult = monitor.getDropResult();
-            if (item && dropResult) placeTile(team, dropResult.row, dropResult.col, edges);
-        },
-        collect: (monitor) => ({
-            opacity: monitor.isDragging() ? 0.4 : 1,
-        }),
-    }), [edges, team]);
-
     return (
-        <div ref={ preview }>
-            <div ref={ drag } style={{ opacity }} className={ edges ? "bg-zinc-900" : "" }>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 75 75">
-                    {
-                        edges ?
-                        [0, 2, 4, 6].map(idx => {
-                            const edge = edges[idx]+edges[idx+1];
-                            const t1 = paths[edge];
-                            const t2 = paths[edge[1]+edge[0]];
-                            return <Edge key={ idx } edge={ edge } color={ t1 ? `fill-${t1}-500` : t2 ? `fill-${t2}-500` : `fill-zinc-100` } />
-                        }) : null
-                    }
-                    { notches.map(n => n) }
-                </svg>
-            </div>
+        <div ref={ ref } className={ edges ? "bg-zinc-900" : "" } { ...props }>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 75 75">
+                {
+                    edges ?
+                    [0, 2, 4, 6].map(idx => {
+                        const edge = edges[idx]+edges[idx+1];
+                        const t1 = paths[edge];
+                        const t2 = paths[edge[1]+edge[0]];
+                        return <Edge key={ idx } edge={ edge } color={ t1 ? `fill-${t1}-500` : t2 ? `fill-${t2}-500` : `fill-zinc-100` } />
+                    }) : null
+                }
+                { notches.map(n => n) }
+            </svg>
         </div>
     )
-}
+})
